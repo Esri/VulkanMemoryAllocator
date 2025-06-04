179a214e5e282ba5811a73d0851de7e3cf5b9300
Christian O. Venegas <cvenegas@esri.com>
2025-06-04 13:33:47 -0700

    Use lf for conan

--- conanfile_rtc.py
+++ conanfile_rtc.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 from conans import ConanFile
 
 
@@ -8,7 +6,7 @@ class VulkanMemoryAllocatorConan(ConanFile):
     version = "3.0.1"
     url = "https://github.com/Esri/VulkanMemoryAllocator/blob/runtimecore/LICENSE.txt"
     license = "https://github.com/Esri/VulkanMemoryAllocator/blob/runtimecore/LICENSE.txt"
-    description = ("Easy to integrate Vulkan memory allocation library.")
+    description = "Easy to integrate Vulkan memory allocation library."
 
     # Use the OS default to get the right line endings
     settings = "os"
